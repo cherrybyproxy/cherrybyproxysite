3103acc15a2cdd08ced2657b1a9c02a039fb6de4
Leah H <leah960319@gmail.com>
2025-08-31 00:04:49 -0400

    eslint leave me alone

--- src/Beyond/Beyond.js
+++ src/Beyond/Beyond.js
@@ -4,7 +4,7 @@ export default function Beyond () {
     return (
         <>
             <div className = {styles.pageContainer}> // "pageContainer"
-                <p className = {styles.title}>Beyond</p>
+                <p className = {styles.title}>Above and Beyond</p>
                 <p className = {styles.text}>what am i without what i love</p>
             </div>
         </>
