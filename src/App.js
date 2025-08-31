8ae36d7281b3637f40edc663bd4d0642c6036450
Leah H <leah960319@gmail.com>
2025-08-31 00:23:11 -0400

    debugging

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import './index.css'
-import './App.css'
-import Navbar from './Navbar'
-import About from './About/About.js'
-import Beyond from './Beyond/Beyond.js'
-import Experience from './Experience/Experience.js'
-import Home from './Home/Home.js'
-import Projects from './Projects/Projects.js'
-import {Route, Routes} from 'react-router-dom'
+// import './index.css'
+// import './App.css'
+// import Navbar from './Navbar'
+// import About from './About/About.js'
+// import Beyond from './Beyond/Beyond.js'
+// import Experience from './Experience/Experience.js'
+// import Home from './Home/Home.js'
+// import Projects from './Projects/Projects.js'
+// import {Route, Routes} from 'react-router-dom'
+
+// function App() {
+//   return (
+//     <>
+//       <Navbar />
+//       <div className = "container">
+//         <Routes>
+//           <Route path = "/" element = {<Home/>}/>
+//           <Route path = "/about" element = {<About/>}/>
+//           <Route path = "/experience" element = {<Experience/>}/>
+//           <Route path = "/projects" element = {<Projects/>}/>
+//           <Route path = "/beyond" element = {<Beyond/>}/>
+//         </Routes>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default App;
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <div className = "container">
-        <Routes>
-          <Route path = "/" element = {<Home/>}/>
-          <Route path = "/about" element = {<About/>}/>
-          <Route path = "/experience" element = {<Experience/>}/>
-          <Route path = "/projects" element = {<Projects/>}/>
-          <Route path = "/beyond" element = {<Beyond/>}/>
-        </Routes>
-      </div>
-    </>
-  );
+  return <div>Hello world</div>;
 }
 
 export default App;
