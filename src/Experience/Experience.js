5217b4283a4e58f3a1fc3267a4c305fe96630cda
Leah H <leah960319@gmail.com>
2025-09-03 17:22:57 -0400

    fixed background

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,10 +1,10 @@
 import styles from './Experience.module.css'
-import cabinInterior from './CabinInterior.PNG'
+// import cabinInterior from './CabinInterior.PNG'
 
 export default function Experience () {
     return (
         <>
-            <img className = {styles.cabinInterior} src = {cabinInterior} alt = 'Cabin Interior' width = '100%' height = '100%'/>
+            {/* <img className = {styles.cabinInterior} src = {cabinInterior} alt = 'Cabin Interior' width = '100%' height = '100%'/> */}
             <div className = {styles.pageContainer}>
                 <p className = {styles.title}>Experience</p>
                 <p className = {styles.text}>hello welcome to my experience fr</p>
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,10 @@
 import styles from './Home.module.css'
-import cabinInterior from './CabinInterior.PNG'
+// import cabinInterior from './CabinInterior.PNG'
 
 export default function Home () {
     return (
         <>
-            <img className = {styles.cabinInterior} src = {cabinInterior} alt = "Cabin Interior" width = "100%" height = "100%"/>
+            {/* <img className = {styles.cabinInterior} src = {cabinInterior} alt = "Cabin Interior" width = "100%" height = "100%"/> */}
             <div className = {styles.pageContainer}>
                 <p className = {styles.text}>Click to explore.</p>
             </div>
