d0c90a5965776dae0c21e5adf56736fd449f42a8
Leah H <leah960319@gmail.com>
2025-09-03 15:34:14 -0400

    more webpages

---
+++ src/Exterior/Exterior.js
@@ -0,0 +1,14 @@
+import styles from './Exterior.module.css'
+import cabinExterior from './CabinExterior.PNG'
+
+export default function Home () {
+    return (
+        <>
+            <img className = {styles.cabinExterior} src = {cabinExterior} alt = "Cabin Exterior" width = "100%" height = "100%"/>
+            <div className = {styles.pageContainer}>
+                <p className = {styles.title}>WELCOME TO LEAH'S CABIN</p>
+                <p className = {styles.text}>Scroll to enter.</p>
+            </div>
+        </>
+    )
+}
