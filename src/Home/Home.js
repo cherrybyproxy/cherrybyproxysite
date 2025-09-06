17c2fea1d9ed1a02d09308314cf6222b21525d31
Leah H <leah960319@gmail.com>
2025-09-05 21:46:35 -0400

    trying something

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,36 @@
 import styles from './Home.module.css'
+// import RamHead from './RamHead.PNG'
+// import {useNavigate} from 'react-router-dom'
 
 export default function Home () {
     return (
         <>
+            {/* <Ram/>
+            <Bag/> */}
             <div className = {styles.pageContainer}>
                 <p className = {styles.text}>Click to explore.</p>
             </div>
         </>
     )
 }
+
+
+// function Ram() {
+//     const navigate = useNavigate()
+//     return (
+//         <button
+//             className={styles.ramClickSpace}
+//             onClick={() => navigate('/About')}
+//         />
+//     )
+// }
+
+// function Bag() {
+//     const navigate = useNavigate()
+//     return (
+//         <button
+//             className={styles.bagClickSpace}
+//             onClick={() => navigate('/Projects')}
+//         />
+//     )
+// }
