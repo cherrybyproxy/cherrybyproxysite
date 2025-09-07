64806fb6e7ef5083727c13baa11da2cbddace77a
Leah H <leah960319@gmail.com>
2025-09-07 00:59:21 -0400

    images

--- src/Beyond/Beyond.js
+++ src/Beyond/Beyond.js
@@ -5,7 +5,7 @@ import Carousel from './Carousel.js'
 export default function Beyond () {
     const carouselimg1 = ["https://imgur.com/McaPxay.jpg", "https://imgur.com/9Gegj5X.jpg"];
     const carouselimg2 = ["https://imgur.com/gYovi1L.jpg", "https://imgur.com/W153Weh.jpg", "https://imgur.com/YZaEkdb.jpg", "https://imgur.com/NTlnDis.jpg", "https://imgur.com/UzjssGe.jpg"]; 
-    const carouselimg3 = ["https://imgur.com/DNxxMEY.jpg", "https://imgur.com/vCFBir6,jpg", "https://imgur.com/ayE6sPP.jpg", "https://imgur.com/TVA6wQZ.jpg", "https://imgur.com/Yg8RlxR.jpg", "https://imgur.com/Rc78ec0.jpg", "https://imgur.com/8qhgB2V.jpg", "https://imgur.com/xWZ5XCG.jpg", "https://imgur.com/JRwTwp8.jpg", "https://imgur.com/Yg8RlxR.jpg"];
+    const carouselimg3 = ["https://imgur.com/DNxxMEY.jpg", "https://imgur.com/vCFBir6,jpg", "https://imgur.com/ayE6sPP.jpg", "https://imgur.com/TVA6wQZ.jpg", "https://imgur.com/Rc78ec0.jpg", "https://imgur.com/8qhgB2V.jpg", "https://imgur.com/xWZ5XCG.jpg", "https://imgur.com/JRwTwp8.jpg", "https://imgur.com/Yg8RlxR.jpg"];
     const carouselimg4 = ["https://imgur.com/2CsXgSk.jpg", "https://imgur.com/66gz4Im.jpg", "https://imgur.com/jF3uAIX.jpg", "https://imgur.com/3jceLAI.jpg", "https://imgur.com/0UKF1Mn.jpg", "https://imgur.com/0jYQo1O.jpg", "https://imgur.com/GaAwr1f.jpg", "https://imgur.com/KCGqjLB.jpg", "https://imgur.com/IddqhSv.jpg", "https://imgur.com/9kqeQcj.jpg"];
     const carouselimg5 = ["https://imgur.com/Ewgiqbx.jpg", "https://imgur.com/fwNLOwF.jpg", "https://imgur.com/CRFoDQW.jpg"];
 
