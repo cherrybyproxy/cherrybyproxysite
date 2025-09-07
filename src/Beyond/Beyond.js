ddc1b24d1f7da121cf485043368ce054fdcc80e8
Leah H <leah960319@gmail.com>
2025-09-07 18:12:49 -0400

    even more images

--- src/Beyond/Beyond.js
+++ src/Beyond/Beyond.js
@@ -2,6 +2,12 @@ import styles from './Beyond.module.css'
 import { useNavigate } from 'react-router-dom'
 import Carousel from './Carousel.js'
 
+import BigRat from './CrochetImages/BigRatImage.heic'
+import Bouquet from './CrochetImages/BouquetImage.heic'
+import SmallRat from './CrochetImages/SmallRatImage.HEIC'
+import Skirt from './CrochetImages/SkirtImage.heic'
+import Bunny from './CrochetImages/BunnyImage.heic'
+
 import Cat from './ClayImages/CatImage.heic'
 import Fish from './ClayImages/FishImage.HEIC'
 import Gift from './ClayImages/GiftImage.HEIC'
@@ -24,9 +30,10 @@ import Raspberry from './BakingImages/StrawberryImage.JPG'
 
 export default function Beyond () {
     const carouselimg1 = ["https://imgur.com/McaPxay.jpg", "https://imgur.com/9Gegj5X.jpg"];
-    const carouselimg2 = ["https://imgur.com/gYovi1L.jpg", "https://imgur.com/W153Weh.jpg", "https://imgur.com/YZaEkdb.jpg", "https://imgur.com/NTlnDis.jpg", "https://imgur.com/UzjssGe.jpg"]; 
+    // const carouselimg2 = ["https://imgur.com/gYovi1L.jpg", "https://imgur.com/W153Weh.jpg", "https://imgur.com/YZaEkdb.jpg", "https://imgur.com/NTlnDis.jpg", "https://imgur.com/UzjssGe.jpg"]; 
     // const carouselimg3 = ["https://imgur.com/DNxxMEY.jpg", "https://imgur.com/vCFBir6,jpg", "https://imgur.com/ayE6sPP.jpg", "https://imgur.com/TVA6wQZ.jpg", "https://imgur.com/Rc78ec0.jpg", "https://imgur.com/8qhgB2V.jpg", "https://imgur.com/xWZ5XCG.jpg", "https://imgur.com/JRwTwp8.jpg", "https://imgur.com/Yg8RlxR.jpg"];
     // const carouselimg4 = ["https://imgur.com/2CsXgSk.jpg", "https://imgur.com/66gz4Im.jpg", "https://imgur.com/jF3uAIX.jpg", "https://imgur.com/3jceLAI.jpg", "https://imgur.com/0UKF1Mn.jpg", "https://imgur.com/0jYQo1O.jpg", "https://imgur.com/GaAwr1f.jpg", "https://imgur.com/KCGqjLB.jpg", "https://imgur.com/IddqhSv.jpg", "https://imgur.com/9kqeQcj.jpg"];
+    const carouselimg2 = [BigRat, Bouquet, SmallRat, Skirt, Bunny];
     const carouselimg3 = [ShinyKey, Monkey, Cat, Fish, Gift, RatBunny, Shell, Keychain, Teeth];
     const carouselimg4 = [Matcha, Brownie, Bun, Carrot, Cinna, Heart, Mochi, Muffin, Raspberry];
     const carouselimg5 = ["https://imgur.com/Ewgiqbx.jpg", "https://imgur.com/fwNLOwF.jpg", "https://imgur.com/CRFoDQW.jpg"];
