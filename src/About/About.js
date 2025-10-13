ca8a037e93a696c3b323d17b3d53d076d3ff20a1
Leah H <leah960319@gmail.com>
2025-10-12 23:57:14 -0400

    update

--- src/About/About.js
+++ src/About/About.js
@@ -63,7 +63,7 @@ export default function About () {
                         <a href = "https://www.linkedin.com/in/leah06/" target = "_blank" rel = "noopener noreferrer">[LINKEDIN]</a>
                         <a href = "mailto:leah960319@gmail.com">[EMAIL]</a>
                     </div>
-                    <p className = {styles.text3}>♬ˎˊ˗ underscores - stupid </p>
+                    <p className = {styles.text3}>♬ˎˊ˗ venturing - Famous girl</p>
                     </div>
                 <img className = {styles.profile} src={Profile} alt="Profile"></img>
             </div>
