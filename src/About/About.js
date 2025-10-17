9b808604c4cdbe5645006b32ee8da5e4fc02a32c
Leah H <leah960319@gmail.com>
2025-10-16 20:23:22 -0400

    update

--- src/About/About.js
+++ src/About/About.js
@@ -63,7 +63,7 @@ export default function About () {
                         <a href = "https://www.linkedin.com/in/leah06/" target = "_blank" rel = "noopener noreferrer">[LINKEDIN]</a>
                         <a href = "mailto:leah960319@gmail.com">[EMAIL]</a>
                     </div>
-                    <p className = {styles.text3}>♬ˎˊ˗ Frost Children, Danny Brown - Shake It Like A</p>
+                    <p className = {styles.text3}>♬ˎˊ˗ Noah Kahan - Call Your Mom</p>
                     </div>
                 <img className = {styles.profile} src={Profile} alt="Profile"></img>
             </div>
